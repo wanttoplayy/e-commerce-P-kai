8acc23b85f67dc1deafd032d1a2b41359534db3a
Vasin Suksuchit <vasinsuk@gosoft.co.th>
2023-07-29 01:20:14 +0700

    almost finish table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,19 +1,25 @@
 "use client";
-
+import { Billboard } from "@prisma/client";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className=" flex items-center justify-between">
         <Heading
-          title="Billboard (0)"
+          title={`Billboard (${data.length})`}
           description="Manage billboard for your store"
         />
         <Button
@@ -24,6 +30,7 @@ export const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable searchKey="label" columns={columns} data={data} />
     </>
   );
 };
