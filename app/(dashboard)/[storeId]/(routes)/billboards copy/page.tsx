5909ed086ea03aadbf6bf2bd16c53ff8008937aa
Vasin Suksuchit <vasinsuk@gosoft.co.th>
2023-07-29 12:13:21 +0700

    copy thing

---
+++ app/(dashboard)/[storeId]/(routes)/billboards copy/page.tsx
@@ -0,0 +1,32 @@
+import { format } from "date-fns";
+
+import { BillboardClient } from "./components/client";
+import prismadb from "@/lib/prismadb";
+import { BillboardColumn } from "./components/columns";
+
+const BillboardsPae = async ({ params }: { params: { storeId: string } }) => {
+  {
+    const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const formattedBillboard: BillboardColumn[] = billboards.map((item) => ({
+      id: item.id,
+      label: item.label,
+      createdAt: format(item.createdAt, "MMMM do, yyyy"),
+    }));
+    return (
+      <div className=" flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <BillboardClient data={formattedBillboard} />
+        </div>
+      </div>
+    );
+  }
+};
+export default BillboardsPae;
