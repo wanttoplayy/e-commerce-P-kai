58d1f459c345cb0a820c97a2521235ee2c750ade
Vasin Suksuchit <vasinsuk@gosoft.co.th>
2023-08-01 21:50:43 +0700

    minor fix afterfinish

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { MainNav } from "@/components/main-nav";
 import StoreSwitcherr from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -21,6 +22,7 @@ const Navbar = async () => {
         <StoreSwitcherr items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
